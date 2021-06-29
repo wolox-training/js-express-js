0d66ace7d6684ed1b84a89887ed179e9f6aa2c35
Juan Sanchez <juan.sanchez@wolox.co>
2021-06-29 15:28:19 -0500

    ·Updated schema rating weet to Validate if weet exists. ·Created weets/rating test.

--- app/middlewares/schemas/rate_weet.js
+++ app/middlewares/schemas/rate_weet.js
@@ -1,9 +1,16 @@
-const { body } = require('express-validator');
+const { body, param } = require('express-validator');
+
 const { scores } = require('../../constants');
+const { getWeetById } = require('../../services/weets');
 
 exports.schemaRateWeet = [
   body('range', 'The range is required and must be -1 or 1')
     .notEmpty()
     .exists()
-    .isIn(Object.values(scores))
+    .isIn(Object.values(scores)),
+  param('id', 'The weet do not exists').custom(async value => {
+    const weet = await getWeetById(value);
+    if (!weet) return Promise.reject(new Error('The weet do not exists'));
+    return true;
+  })
 ];
